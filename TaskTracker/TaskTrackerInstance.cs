2db70b38333cb8ad3c8f745dc3f503b489dcf3c4
CeniusM <cenius.meier@gmail.com>
2023-05-12 13:03:11 +0200

    Add project files.

---
+++ TaskTracker/TaskTrackerInstance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker;
+
+/// <summary>
+/// Class used for loading and saving tasks, and as an API to change and veiw the tasks at hand.
+/// </summary>
+public class TaskTrackerInstance
+{
+}
